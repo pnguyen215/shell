e8c60f18d92232ee2dddf80b84a552a96b2782f5
arisnguyenit97 <nguyenhuuphuocit97@gmail.com>
2025-02-26 23:22:11 +0700

    :mute: silent changes: update codebase #1

--- src/shell.sh
+++ src/shell.sh
@@ -17,3 +17,11 @@ fi
 shell_version() {
     echo "shell v0.1.0"
 }
+
+shell_upgrade() {
+    echo "🚀 Upgrading shell..."
+    install_dir="$HOME/shell"
+    [ -d "$install_dir" ] && rm -rf "$install_dir"
+    bash -c "$(curl -fsSL https://raw.githubusercontent.com/pnguyen215/shell/master/install.sh)"
+    echo "🍺 shell upgraded. Restart your terminal or run 'source ~/.zshrc' or 'source ~/.bashrc' to apply changes."
+}
