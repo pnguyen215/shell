fb7d05d69d7b50e86d03d87168ae0404cbdd4e0d
arisnguyenit97 <nguyenhuuphuocit97@gmail.com>
2025-02-27 01:06:38 +0700

    :sparkles: feat: add function #1

--- src/lib/homebrew.sh
+++ src/lib/homebrew.sh
@@ -4,3 +4,14 @@
 function install_homebrew() {
     run_cmd_eval '/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"'
 }
+
+function uninstall_homebrew() {
+    if is_command_available brew; then
+        echo "🚀 Uninstalling Homebrew..."
+        run_cmd_eval '/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/uninstall.sh)"'
+        run_cmd_eval 'sed -i.bak '/# Homebrew/d' "$HOME/.zprofile"' # Remove Homebrew-related lines from the shell profile
+        echo "🟢 Homebrew uninstalled successfully!"
+    else
+        echo "🟡 Homebrew is not installed. Nothing to uninstall."
+    fi
+}
