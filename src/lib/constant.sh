6fb586bd9bf9c67c1502b183be651e9b4be29f55
arisnguyenit97 <nguyenhuuphuocit97@gmail.com>
2025-03-30 20:53:00 +0700

    :recycle: refactor: update codebase #2

--- src/lib/constant.sh
+++ src/lib/constant.sh
@@ -8,7 +8,7 @@
 #
 # Example:
 #   SHELL_CONF_WORKING="$HOME/.shell-config"
-declare -r SHELL_CONF_WORKING="$HOME/.shell-config"
+SHELL_CONF_WORKING="$HOME/.shell-config"
 
 # SHELL_CONF_WORKING_WORKSPACE constant
 # This variable defines the path to the workspace directory within the shell configuration working directory.
@@ -17,7 +17,7 @@ declare -r SHELL_CONF_WORKING="$HOME/.shell-config"
 #
 # Example:
 #   SHELL_CONF_WORKING_WORKSPACE="$SHELL_CONF_WORKING/workspace"
-declare -r SHELL_CONF_WORKING_WORKSPACE="$SHELL_CONF_WORKING/workspace"
+SHELL_CONF_WORKING_WORKSPACE="$SHELL_CONF_WORKING/workspace"
 
 # SHELL_KEY_CONF_FILE constant
 # This variable defines the path to the key configuration file used by the shell bash library.
@@ -28,7 +28,7 @@ declare -r SHELL_CONF_WORKING_WORKSPACE="$SHELL_CONF_WORKING/workspace"
 #
 # Example:
 #   SHELL_KEY_CONF_FILE="$HOME/.shell-config/key.conf"
-declare -r SHELL_KEY_CONF_FILE="$SHELL_CONF_WORKING/key.conf"
+SHELL_KEY_CONF_FILE="$SHELL_CONF_WORKING/key.conf"
 
 # SHELL_KEY_CONF_FILE_WORKSPACE constant
 # This variable defines the path to the profile-specific configuration file within the workspace directory.
@@ -37,7 +37,7 @@ declare -r SHELL_KEY_CONF_FILE="$SHELL_CONF_WORKING/key.conf"
 #
 # Example:
 #   SHELL_KEY_CONF_FILE_WORKSPACE="$SHELL_CONF_WORKING_WORKSPACE/profile.conf"
-declare -r SHELL_KEY_CONF_FILE_WORKSPACE="$SHELL_CONF_WORKING_WORKSPACE/profile.conf"
+SHELL_KEY_CONF_FILE_WORKSPACE="$SHELL_CONF_WORKING_WORKSPACE/profile.conf"
 
 # SHELL_KEY_CONF_SETTING_WORKSPACE constant
 # This variable defines the path to the settings configuration file within the workspace directory.
@@ -46,7 +46,7 @@ declare -r SHELL_KEY_CONF_FILE_WORKSPACE="$SHELL_CONF_WORKING_WORKSPACE/profile.
 #
 # Example:
 #   SHELL_KEY_CONF_SETTING_WORKSPACE="$SHELL_CONF_WORKING_WORKSPACE/settings.conf"
-declare -r SHELL_KEY_CONF_SETTING_WORKSPACE="$SHELL_CONF_WORKING_WORKSPACE/settings.conf"
+SHELL_KEY_CONF_SETTING_WORKSPACE="$SHELL_CONF_WORKING_WORKSPACE/settings.conf"
 
 # SHELL_GROUP_CONF_FILE constant
 # This variable defines the path to the group configuration file used by the shell bash library.
@@ -58,7 +58,7 @@ declare -r SHELL_KEY_CONF_SETTING_WORKSPACE="$SHELL_CONF_WORKING_WORKSPACE/setti
 #
 # Example:
 #   SHELL_GROUP_CONF_FILE="$HOME/.shell-config/group.conf"
-declare -r SHELL_GROUP_CONF_FILE="$SHELL_CONF_WORKING/group.conf"
+SHELL_GROUP_CONF_FILE="$SHELL_CONF_WORKING/group.conf"
 
 # SHELL_GH_CONF_FILE constant
 # This variable defines the path to the GitHub configuration file used by the shell bash library.
@@ -67,7 +67,7 @@ declare -r SHELL_GROUP_CONF_FILE="$SHELL_CONF_WORKING/group.conf"
 #
 # Example:
 #   SHELL_GH_CONF_FILE="$HOME/.shell-config/gh.conf"
-declare -r SHELL_GH_CONF_FILE="$SHELL_CONF_WORKING/gh.conf"
+SHELL_GH_CONF_FILE="$SHELL_CONF_WORKING/gh.conf"
 
 # SHELL_PROTECTED_KEYS array
 # This array lists configuration keys that are considered constant and must not be removed, updated,
@@ -77,7 +77,7 @@ declare -r SHELL_GH_CONF_FILE="$SHELL_CONF_WORKING/gh.conf"
 #
 # Example:
 #   SHELL_PROTECTED_KEYS=("HOST" "PORT" "API_TOKEN")
-declare -r -a SHELL_PROTECTED_KEYS=("HOST" "PORT" "SHELL_DEVELOPER" "SHELL_HISTORICAL_GH_TELEGRAM_BOT_TOKEN" "SHELL_HISTORICAL_GH_TELEGRAM_CHAT_ID")
+SHELL_PROTECTED_KEYS=("HOST" "PORT" "SHELL_DEVELOPER" "SHELL_HISTORICAL_GH_TELEGRAM_BOT_TOKEN" "SHELL_HISTORICAL_GH_TELEGRAM_CHAT_ID")
 
 # SHELL_C_AES_RED constant
 # This variable defines the ANSI escape code for red text color. It is used to highlight error messages or critical alerts in the shell.
