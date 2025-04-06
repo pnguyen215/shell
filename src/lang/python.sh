ff2e0101c423fa54dece2d8ec1e3cedbc6aae900
arisnguyenit97 <nguyenhuuphuocit97@gmail.com>
2025-04-06 22:41:28 +0700

    :sparkles: feat: add base shell::add_python_gitignore function #4

--- src/lang/python.sh
+++ src/lang/python.sh
@@ -1260,3 +1260,17 @@ shell::pip_install_requirements_env() {
         fi
     fi
 }
+
+# shell::add_python_gitignore function
+# This function downloads the .gitignore file specifically for Python projects.
+#
+# The .gitignore file is essential for specifying which files and directories
+# should be ignored by Git, helping to keep the repository clean and free of
+# unnecessary files that do not need to be tracked.
+#
+# It utilizes the shell::download_dataset function to fetch the .gitignore file
+# from the specified URL and saves it in the appropriate location within the
+# project structure.
+shell::add_python_gitignore() {
+    shell::download_dataset ".gitignore" $SHELL_PROJECT_GITIGNORE_PYTHON
+}
