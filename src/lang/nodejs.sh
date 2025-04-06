f0147d2fc230e87fd5f3fb3b05c0256f13872a40
arisnguyenit97 <nguyenhuuphuocit97@gmail.com>
2025-04-06 22:39:49 +0700

    :sparkles: feat: add base shell::add_nodejs_gitignore function #4

---
+++ src/lang/nodejs.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# nodejs.sh
+
+# shell::add_nodejs_gitignore function
+# This function downloads the .gitignore file specifically for Node.js projects.
+#
+# The .gitignore file is crucial for specifying which files and directories
+# should be ignored by Git, ensuring that the repository remains clean and
+# free of unnecessary files that do not need to be tracked.
+#
+# It utilizes the shell::download_dataset function to fetch the .gitignore file
+# from the specified URL and saves it in the appropriate location within the
+# project structure.
+shell::add_nodejs_gitignore() {
+    shell::download_dataset ".gitignore" $SHELL_PROJECT_GITIGNORE_NODEJS
+}
