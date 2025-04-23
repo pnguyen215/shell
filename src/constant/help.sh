46a6d334b883a2c7b2d2ca5609c6fd902d3d0e69
arisnguyenit97 <nguyenhuuphuocit97@gmail.com>
2025-04-23 13:47:18 +0700

    :books: docs: add help message shell::remove_bookmark_linux function #2

--- src/constant/help.sh
+++ src/constant/help.sh
@@ -1262,3 +1262,20 @@ Description:
   and then replaces the original bookmarks file with the filtered version.
   If the bookmark is not found or removal fails, an error message is displayed.
 "
+
+USAGE_SHELL_REMOVE_BOOKMARK_LINUX="
+shell::remove_bookmark_linux function
+Deletes a bookmark with the specified name from the bookmarks file.
+
+Usage:
+  shell::remove_bookmark_linux [-h] <bookmark_name>
+
+Parameters:
+  - -h              : Optional. Displays this help message.
+  - <bookmark_name> : The name of the bookmark to remove.
+
+Description:
+  This function searches for a bookmark entry in the bookmarks file that ends with \"|<bookmark_name>\".
+  If the entry is found, it uses sed to delete the line from the file.
+  The sed command is constructed differently for macOS and Linux due to differences in the in-place edit flag.
+"
