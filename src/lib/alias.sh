87d1f800f7f077b117ae15c13f8a38d6443db9b3
arisnguyenit97 <nguyenhuuphuocit97@gmail.com>
2025-05-26 21:50:42 +0700

    :recycle: refactor: update codebase #2

--- src/lib/alias.sh
+++ src/lib/alias.sh
@@ -6,6 +6,7 @@ alias sve="shell::version"
 alias sup="shell::upgrade"
 alias sac="shell::add_conf"
 alias sgc="shell::fzf_get_conf"
+alias snv="shell::fzf_goto_verifier" # navigate check path
 
 # Kernel alias
 alias c="clear"
