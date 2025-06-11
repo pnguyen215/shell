a1b2fe4caf775dced51a556e21d4259525bec6a0
arisnguyenit97 <nguyenhuuphuocit97@gmail.com>
2025-06-11 21:49:58 +0700

    :sparkles: feat: add shell::sanitize_first_upper_var_name and shell::sanitize_first_lower_var_name function #4

--- src/lib/strings.sh
+++ src/lib/strings.sh
@@ -44,3 +44,46 @@ shell::sanitize_lower_var_name() {
     # Convert to lowercase, replace non-alphanumeric and non-underscore with underscore
     echo "$input" | tr '[:upper:]' '[:lower:]' | sed -e 's/[^a-z0-9_]/_/g'
 }
+
+# shell::sanitize_first_upper_var_name function
+# Converts a string into a format suitable for an environment variable name,
+# capitalizing the first letter and replacing non-alphanumeric characters
+# (except underscore) with underscores.
+#
+# Usage:
+#   shell::sanitize_first_upper_var_name <string>
+#
+# Parameters:
+#   - <string> : The input string (e.g., INI section or key name).
+#
+# Returns:
+#   The sanitized string suitable for a shell variable name with the first letter capitalized.
+#
+# Example:
+#   sanitized=$(shell::sanitize_first_upper_var_name "my-section.key_name") # Outputs "My_section_key_name"
+shell::sanitize_first_upper_var_name() {
+    local input="$1"
+    # Convert first character to uppercase, replace non-alphanumeric and non-underscore with underscore
+    echo "$input" | sed -e 's/^\(.\)/\U\1/' -e 's/[^A-Z0-9_]/_/g'
+}
+
+# shell::sanitize_first_lower_var_name function
+# Converts a string into a format suitable for a lower-case environment variable name,
+# making the first letter lowercase and replacing non-alphanumeric characters
+# (except underscore) with underscores.
+# Usage:
+#   shell::sanitize_first_lower_var_name <string>
+#
+# Parameters:
+#   - <string> : The input string (e.g., INI section or key name).
+#
+# Returns:
+#   The sanitized string suitable for a lower-case shell variable name with the first letter lowercase.
+#
+# Example:
+#   sanitized=$(shell::sanitize_first_lower_var_name "My-Section.Key_Name") # Outputs "my_section_key_name"
+shell::sanitize_first_lower_var_name() {
+    local input="$1"
+    # Convert first character to lowercase, replace non-alphanumeric and non-underscore with underscore
+    echo "$input" | sed -e 's/^\(.\)/\L\1/' -e 's/[^a-z0-9_]/_/g'
+}
---
+++ src/lib/vpn.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+# vpn.sh
