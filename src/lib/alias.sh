a6fb90fda36c530080fa36f1b0cf972b500cf11f
arisnguyenit97 <nguyenhuuphuocit97@gmail.com>
2025-07-01 00:07:11 +0700

    :recycle: refactor: update codebase #2

--- src/lib/alias.sh
+++ src/lib/alias.sh
@@ -6,18 +6,21 @@ alias sve="shell::version"
 alias sup="shell::upgrade"
 alias sac="shell::add_key_conf"
 alias sgc="shell::fzf_get_key_conf"
-alias snv="shell::fzf_list_bookmark_up"                 # list bookmarks with fzf
-alias sre-boo="shell::fzf_rename_bookmark"              # rename bookmark
-alias sre-boo-n="shell::fzf_rename_bookmark -n"         # rename bookmark with dry mode
-alias srm-boo="shell::fzf_remove_bookmark"              # remove bookmark
-alias srm-boo-n="shell::fzf_remove_bookmark -n"         # remove bookmark with dry mode
-alias sec="shell::editor .shell-config"                 # edit shell config
-alias sec-n="shell::editor -n .shell-config"            # edit shell config with dry mode
-alias srd="shell::fzf_rename_dir_base_bookmark"         # rename dir base bookmark
-alias srd-n="shell::fzf_rename_dir_base_bookmark -n"    # rename dir base bookmark with dry mode
-alias sgemcheck='shell::ask_gemini_english'             # check grammar english by Gemini
-alias sgrammarai='shell::ask_gemini_english'            # check grammar english by Gemini
-alias sgemcheck-async='shell::async "sgemcheck \"$*\""' # check grammar english by Gemini async
+alias snv="shell::fzf_list_bookmark_up"              # list bookmarks with fzf
+alias sre-boo="shell::fzf_rename_bookmark"           # rename bookmark
+alias sre-boo-n="shell::fzf_rename_bookmark -n"      # rename bookmark with dry mode
+alias srm-boo="shell::fzf_remove_bookmark"           # remove bookmark
+alias srm-boo-n="shell::fzf_remove_bookmark -n"      # remove bookmark with dry mode
+alias sec="shell::editor .shell-config"              # edit shell config
+alias sec-n="shell::editor -n .shell-config"         # edit shell config with dry mode
+alias srd="shell::fzf_rename_dir_base_bookmark"      # rename dir base bookmark
+alias srd-n="shell::fzf_rename_dir_base_bookmark -n" # rename dir base bookmark with dry mode
+alias sgemcheck='shell::ask_gemini_english'          # check grammar english by Gemini
+alias sgrammarai='shell::ask_gemini_english'         # check grammar english by Gemini
+# alias sgemcheck-async='shell::async "sgemcheck \"$*\""' # check grammar english by Gemini async
+sgemcheck-async() {
+    shell::async "sgemcheck \"$*\""
+}
 
 # Kernel alias
 alias c="clear"
