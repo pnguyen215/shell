4ea8f231df3e89dcde31c2bee36505fab934dbb3
arisnguyenit97 <nguyenhuuphuocit97@gmail.com>
2025-07-22 22:15:01 +0700

    :recycle: refactor: update codebase #2

--- src/devops/ci.sh
+++ src/devops/ci.sh
@@ -33,3 +33,18 @@ shell::add_gh_wrk_news() {
     fi
     shell::download_dataset ".github/workflows/gh_wrk_news.yml" $SHELL_PROJECT_GITHUB_WORKFLOW_NEWS
 }
+
+# shell::add_gh_wrk_sh_pretty function
+# This function downloads the GitHub Actions workflow configuration file for shell script
+# formatting from the specified GitHub repository. This file is essential for
+# automating the formatting of shell scripts in the project, ensuring consistency and
+# adherence to coding standards.
+# It utilizes the shell::download_dataset function to fetch the file and save it
+# in the appropriate location within the project structure.
+shell::add_gh_wrk_sh_pretty() {
+    if [ "$1" = "-h" ]; then
+        echo "$USAGE_SHELL_ADD_GH_WRK_SH_PRETTY"
+        return 0
+    fi
+    shell::download_dataset ".github/workflows/gh_wrk_sh_pretty.yml" $SHELL_PROJECT_GITHUB_WORKFLOW_SH_PRETTY
+}
