1c1bf2027d2c933806b7613f20c03d70822fa047
arisnguyenit97 <nguyenhuuphuocit97@gmail.com>
2025-07-26 21:04:18 +0700

    :recycle: refactor: update codebase #2

--- src/lib/ini.sh
+++ src/lib/ini.sh
@@ -775,8 +775,10 @@ shell::write_ini() {
 	while IFS= read -r line || [ -n "$line" ]; do
 		# Trim leading and trailing whitespace from the line for easier processing.
 		local trimmed_line
+		set +x
 		trimmed_line="$(echo "$line" | sed 's/^[[:space:]]*//;s/[[:space:]]*$//')"
 		# trimmed_line=$(awk '{gsub(/^[[:space:]]*/, "", $0); gsub(/[[:space:]]*$/, "", $0); print $0}' <<<"$line")
+		# set -x
 
 		# Skip empty lines (after trimming). This removes blank lines within sections.
 		if [ -z "$trimmed_line" ]; then
