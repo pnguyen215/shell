2374540ebe4c330041dd984bde08bb7f9a4af223
arisnguyenit97 <nguyenhuuphuocit97@gmail.com>
2025-08-10 14:42:46 +0700

    :recycle: refactor: update codebase #2

--- src/lib/logger.sh
+++ src/lib/logger.sh
@@ -292,7 +292,6 @@ shell::logger::option() {
 		return 0
 	fi
 	
-	shell::colored_echo "OPTIONS:" 39
 	if [[ -n "$description" ]]; then
 		local formatted_line
 		formatted_line=$(printf "    %-20s %s" "$option" "$description")
