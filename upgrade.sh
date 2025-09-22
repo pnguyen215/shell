29ab3aae02e9ecc4bb4a6047f8557198f10226e6
arisnguyenit97 <nguyenhuuphuocit97@gmail.com>
2025-09-22 15:18:28 +0700

    :recycle: refactor: update codebase #2

--- upgrade.sh
+++ upgrade.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
-echo "🚀 Upgrading shell..."
-install_dir="$HOME/shell"
-[ -d "$install_dir" ] && rm -rf "$install_dir"
+# upgrade.sh
+
+echo "↻ Upgrading shell..."
+local shell_pkg="$HOME/shell"
+
+# Remove the old shell package if it exists
+if [ -d "$shell_pkg" ]; then
+	rm -rf "$shell_pkg"
+fi
+
+# Install the latest version of the shell
 bash -c "$(curl -fsSL https://raw.githubusercontent.com/pnguyen215/shell/master/install.sh)"
+
 echo "INFO: shell upgraded. Restart your terminal or run 'source ~/.zshrc' or 'source ~/.bashrc' to apply changes."
